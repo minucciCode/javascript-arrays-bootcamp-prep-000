84adafb300a1a44367758c0f79bd90e27fc3aea3
Gianluca Minucci <glminucci@gmail.com>
2018-09-14 23:04:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index) {
     return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   array.shift(element)
   return array
 }
