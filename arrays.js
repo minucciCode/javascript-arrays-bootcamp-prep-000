0971600ae3bce58b68d484356c0275345e4d2e2c
Gianluca Minucci <glminucci@gmail.com>
2018-09-14 22:32:01 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,37 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+    return [element, ...array]
+}
+/*
+function destructivelyAddElementToBeginningOfArray(array, element) {
+    return 
+}
+
+function addElementToEndOfArray(array, element)
+{
+  array = [...array, element];
+  {
+    return array
+  }
+}
+addElementToEndOfArray()
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element);
+  {
+    return array;
+  }
+}
+destructivelyAddElementToEndOfArray()
+
+function accessElementInArray(array, index)
+{
+  array = [element1, element2, element3]
+  {
+    return array[2]
+  }
+}
+accessElementInArray()
+*/
