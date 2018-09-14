2df1d6329c141b81400a2c9cc91309882415cee4
Gianluca Minucci <glminucci@gmail.com>
2018-09-14 23:09:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  return array
+  return [array.slice(1)]
 }
 
