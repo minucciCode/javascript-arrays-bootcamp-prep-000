ed93f578a9ec86fcaa17b4d561c4b1cd7b101a75
Gianluca Minucci <glminucci@gmail.com>
2018-09-14 23:25:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,4 +36,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-function removeElementFromEndOfArray
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+  return array
+}
