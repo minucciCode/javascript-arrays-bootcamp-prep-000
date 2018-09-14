9405d6d9f88f44446f834b8d461a112723b4b402
Gianluca Minucci <glminucci@gmail.com>
2018-09-14 22:49:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  array = [element1, element2, element3]
-    return 
+    return array[0]
 }
 /*
